66160b758cb5da564e6bded09be49533ed8f2b31
Luis Mejias <l.mejias@avoristravel.com>
2025-02-17 10:51:09 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'dashboard',
+  },
+];
