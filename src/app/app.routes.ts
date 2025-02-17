351be5233ae5b3cf639c8f7c7188548003d3b282
Luis Mejias <l.mejias@avoristravel.com>
2025-02-17 12:36:52 -0300

    added home and several assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages';
 export const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home page',
+  },
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: 'dashboard',
+    redirectTo: 'home',
   },
 ];
